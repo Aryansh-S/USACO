dc46f0b69b798b577ab9c0633da922809d7a65e4
Aryansh-S <63766132+Aryansh-S@users.noreply.github.com>
2020-04-15 23:23:21 -0700

    Update diffarray.cpp

--- Implementations/Algorithms/DP/diffarray.cpp
+++ Implementations/Algorithms/DP/diffarray.cpp
@@ -12,7 +12,7 @@ int n, u, q, data[SZ], diffdp[SZ];
 
 int main(){
   cin >> n >> q; F0R(i,n) cin >> data[i];
-  FOR(i,1,n+1) diffdp[i]=data[i]-data[i-1]; //draw out into differences (carry and drop off)
+  FOR(i,1,n) diffdp[i]=data[i]-data[i-1]; //draw out into differences (carry and drop off)
   F0R(i,u){int a,b; cin >> a >> b; a--,b--; diffdp[a]++; diffdp[b+1]--;} //perform updates
   F0R(i,n-1) diffdp[i+1]+=diffdp[i]; //collapse back w/ psums 
   F0R(i,q){int a; cin >> a; a--; cout << diffdp[a] << "\n";}
