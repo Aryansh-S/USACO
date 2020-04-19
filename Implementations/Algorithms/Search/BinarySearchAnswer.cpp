e2c1a1aef0b7f543c05f364b4ec154a2d48d47b3
Aryansh-S <63766132+Aryansh-S@users.noreply.github.com>
2020-04-18 19:01:48 -0700

    Create BinarySearchAnswer.cpp

---
+++ Implementations/Algorithms/Search/BinarySearchAnswer.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,ans;
+
+int checkifworks(int k){
+  // return 1 if k works, -1 if k too small, 0 if k too big (based on some monotonic conditions)
+}
+
+int main(){
+  cin >> n; 
+  //check for k from 0 to n-1
+  int l=0,r=n-1;
+  while(l<=r){
+    int m=l+(r-l)/2;
+    if(check(m)==1){ans=m; break;}
+    else if(check(m)==-1 && check(m+1)==0) {ans=m; break;}
+    else if(check(m)==0 && check(m+1)==-1) {ans=m+1; break;}
+    else if(check(m)==-1) l=m+1;
+    else r=m-1;
+  }
+  cout << ans << "\n";
+  return 0;
+}
