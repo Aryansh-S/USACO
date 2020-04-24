58bcfa1cca68a2cf5c3d12f59b0f1c06a6fae4bc
Aryansh-S <63766132+Aryansh-S@users.noreply.github.com>
2020-04-24 16:35:48 -0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 /*
  * Aryansh Shrivastava 
  * USACO Template 
- * Last Updated: 4/22/20
+ * Last Updated: 4/24/20
  */
 
 #include <bits/stdc++.h> 
@@ -20,12 +20,10 @@ typedef long long ll;
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
 typedef vector<int> vi;
-#define maxpq priority_queue
-#define maxpqi maxpq<int> 
-#define minpqi maxpq<int, vi, greater<int> >
+template<class T> using minpq = priority_queue<T, vector<T>, greater<T> >;
+template<class T> using maxpq = priority_queue<T>;
 #define pb push_back
 #define ins insert
-#define ers erase
 #define mp make_pair
 #define f first
 #define s second
@@ -36,8 +34,8 @@ typedef vector<int> vi;
 #define all(x) begin(x),end(x)
 #define trav(a,x) for(auto&(a):(x))
 #define FOR(i,a,b) for(__typeof(b)i=(a)-((a)>(b));i!=(b)-((a)>(b));i+=((a)>(b))?-1:1) //go from i=lb to i=ub-1
-#define FOR_(i,a,b,d) for(__typeof(b)i=(a)-((a)>(b));(a>b)?(i>b-1):(i<b);i+=(((a)>(b))?-1:1)*abs(d)) //go from i=lb to i=ub-1 by size d increments
 #define F0R(i,b) FOR(i,0,b)
+#define FOR_(i,a,b,d) for(__typeof(b)i=(a)-((a)>(b));(a>b)?(i>b-1):(i<b);i+=(((a)>(b))?-1:1)*abs(d)) //go from i=lb to i=ub-1 by size d increments
 #define F0R_(i,b,d) FOR_(i,0,b,d)
 #define R0F(i,n) FOR(i,n,0)
 #define R0F_(i,b,d) FOR_(i,b,0,d)
@@ -51,6 +49,8 @@ typedef vector<int> vi;
 #define INF 0x3f3f3f3f
 #define INFLL 0x3f3f3f3f3f3f3f3f
 #define EPS 1e-8
+#define ckmin(a,b) a=min(a,b)
+#define ckmax(a,b) a=max(a,b)
 
 void in(){} void outln(){} void out_(){cout << "\n";} //for fast, easy IO
 template<typename T, typename...Types> void in(T& var1, Types&...var2){cin >> var1; in(var2...);}
