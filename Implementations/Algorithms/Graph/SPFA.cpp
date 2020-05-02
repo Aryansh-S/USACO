f4e40f236d75b7d8e8746063de2a50a6b592bd52
Aryansh-S <63766132+Aryansh-S@users.noreply.github.com>
2020-05-01 22:39:41 -0700

    Update SPFA.cpp

--- Implementations/Algorithms/Graph/SPFA.cpp
+++ Implementations/Algorithms/Graph/SPFA.cpp
@@ -6,91 +6,62 @@ using namespace std;
 //this is not the traditional implementation of SPFA in research discipline; we maintain a double-ended queue (deque) to 
 //speed up the process
 
-typedef long long ll;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
-typedef vector<int> vi;
-template<class T> using minpq = priority_queue<T, vector<T>, greater<T> >;
-template<class T> using maxpq = priority_queue<T>;
-#define pb push_back
-#define ins insert
-#define mp make_pair
+typedef vector<int> vi; 
+typedef pair<int,int> pii; 
 #define f first
 #define s second
-#define sz(x) (int)(x).size()
-#define rsz resize
-#define lb lower_bound
-#define ub upper_bound
-#define all(x) begin(x),end(x)
-#define trav(a,x) for(auto&(a):(x))
+#define mp make_pair
+#define pb push_back
 #define FOR(i,a,b) for(__typeof(b)i=(a)-((a)>(b));i!=(b)-((a)>(b));i+=((a)>(b))?-1:1) //go from i=lb to i=ub-1
 #define F0R(i,b) FOR(i,0,b)
-#define FOR_(i,a,b,d) for(__typeof(b)i=(a)-((a)>(b));(a>b)?(i>b-1):(i<b);i+=(((a)>(b))?-1:1)*abs(d)) //go from i=lb to i=ub-1 by size d increments
-#define F0R_(i,b,d) FOR_(i,0,b,d)
-#define R0F(i,n) FOR(i,n,0)
-#define R0F_(i,b,d) FOR_(i,b,0,d)
-#define F1R(i,b) FOR(i,1,b+1)
-#define R1F(i,b) FOR(i,b+1,1)
-#define F1R_(i,b,d) FOR_(i,1,b+1,d)
-#define R1F_(i,b,d) FOR_(i,b+1,1,d)
-#define what(x) out((#x),"is",(x))
-#define big (int)1e5
-#define bigg (int)(1e9+7)
+#define trav(a,x) for(auto&(a):(x))
 #define INF 0x3f3f3f3f
-#define INFLL 0x3f3f3f3f3f3f3f3f
-#define EPS 1e-8
-#define ckmin(a,b) a=min(a,b)
-#define ckmax(a,b) a=max(a,b)
+#define all(v) begin(v),end(v)
 
-void in(){} void outln(){} void out_(){cout << "\n";} //for fast, easy IO
-template<typename T, typename...Types> void in(T& var1, Types&...var2){cin >> var1; in(var2...);}
-template<typename T> void out(T var1){cout << var1 << "\n";}
-template<typename T, typename...Types> void out(T var1, Types...var2){cout << var1 << " "; out(var2...);}
-template<typename T, typename...Types> void outln(T var1, Types...var2){cout << var1 << "\n"; outln(var2...);}
-template<typename T> void out_(T var1){cout << var1 << " ";}
-
-bool comps(const pii&p1, const pii&p2){return (p1.s!=p2.s)?(p1.s<p2.s):(p1.f<p2.f);} //sort and search with special pair conditions & compare int-pair
-struct intpairf{bool operator()(const pii&a, const int&b){return(a.f!=b)?(a.f<b):(a.s<b);}
-bool operator()(const int&a, const pii&b){return(a!=b.f)?(a<b.f):(a<b.s);}};
-struct intpairs{bool operator()(const pii&a, const int&b){return(a.s!=b)?(a.s<b):(a.f<b);}
-bool operator()(const int&a, const pii&b){return(a!=b.s)?(a<b.s):(a<b.f);}};
-
-#define PNAME ""
-
-template <class T, int SZ> struct SPFA{ //optimized Bellman-Ford that *may* run linear time for some cases
-//class T weight
-    vector<pair<T,int> > adj[SZ]; T dist[SZ]; bool inq[SZ]; int mom[SZ]; 
-    void add(int a, int b, T w){adj[a].pb(mp(w,b)); adj[b].pb(mp(w,a));}
-	void add_(int a, int b, T w){adj[a].pb(mp(w,b));} //if edge is directed
-    void upd(int start){
-        F0R(i,SZ) dist[i]=(i!=start)?INF:0,inq[i]=0;
-        queue<pair<T, int> > q; q.push(mp(0,start));
-        inq[start]=1;
+template <class T, int SZ> struct SPFA{ //average O(E), wc bell-ford, check for negcyc
+    vector<pair<T,int> > adj[SZ]; int mom[SZ],r[SZ]; T dist[SZ]; bool negcycbad;
+    bool inq[SZ];
+    void add(int a, int b, T w){
+        adj[a].pb(mp(w,b)); adj[b].pb(mp(w,a));
+    }
+    void add_(int a, int b, T w){adj[a].pb(mp(w,b));}
+    void upd(int start=0){
+        F0R(i,SZ) dist[i]=(i!=start)?INF:0, mom[i]=i, inq[i]=0;
+        deque<int> q; q.pb(start); inq[start]=1;
         while(!q.empty()){
-            auto curr=q.front(); q.pop(); inq[curr.s]=0;
-            trav(v,adj[curr.s]){
-                what(sz(q)); exit(0);
-                if(dist[v.s]>dist[curr.s]+v.f){
-                    dist[v.s]=dist[curr.s]+v.f;
-                    if(!inq[v.s]){inq[v.s]=1; q.push(mp(v.f,v.s));}
+            int curr=q.front(); q.pop_front(); inq[curr]=0;
+            trav(v,adj[curr]){
+                if(dist[v.s]>dist[curr]+v.f){
+                    dist[v.s]=dist[curr]+v.f; mom[v.s]=curr; r[v.s]++;
+                    if(r[v.s]==SZ){negcycbad=1; break;} //can't relax so much
+                    if(!inq[v.s]){
+                        if(q.empty()) q.pb(v.s);
+                        if(dist[q.front()]>dist[v.s]) q.push_front(v.s);
+                        else q.pb(v.s);
+                    }
                 }
             }
         }
-        out("TEST"); exit(0);
     }
     T query(int v){return dist[v];}
-	T query_(int st, int v){upd(st); return query(v);}
+    T query_(int st, int v){upd(st); return query(v);}
+    vector<pii> path(int v){
+        vector<pii> ret; int i=v; 
+        while(mom[i]!=i){ret.pb(mp(mom[i],i)); i=mom[i];}
+        reverse(all(ret)); return
+    }
+    vector<pii> path_(int st, int v){upd(st); return path(v);}
+    bool negcyc(){return negcycbad;}
 };
 
-SPFA<int,3> s;
+SPFA<int,5> s;
 
 int main(){
-	cin.tie(0)->sync_with_stdio(0); if(fopen(PNAME ".in","r")){freopen(PNAME ".in","r",stdin); freopen(PNAME ".out","w",stdout);}
-    
-    s.add(0,1,-1);
-    s.add(1,2,-5);
-    s.add(0,2,0);
-    out(s.query_(0,2));
-
-	return 0;
+    s.add_(0,1,-1);
+    s.add_(1,2,-2);
+    s.add_(0,2,0);
+    s.upd(0);
+    auto a=s.path(2);
+    trav(t,a) cout << t.f << " " << t.s << "\n";
+    return 0;
 }
