b2272ff787c8540f6f83d38b10dc7973d6b6759c
Aryansh-S <63766132+Aryansh-S@users.noreply.github.com>
2020-05-01 23:04:52 -0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -72,46 +72,33 @@ void rsort(int*l,int*r,int msb=31){
     if(l!=r&&msb>=0){vi::iterator m=partition(l,r,radix(msb)); msb--;rsort(l,m,msb),rsort(m,r,msb);}
 }
 
-struct DSU{ //from 0 to n
+struct DSU{ //from 0 to n-1
 	vi e; void init(int n) { e = vi(n,-1); }
+    bool rollback; vector<array<int,4>> mod;
 	int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); }
 	bool sameSet(int a, int b) { return get(a) == get(b); }
 	int size(int x) { return -e[get(x)]; }
 	bool unite(int x, int y) { // union-by-rank
-		x = get(x), y = get(y); if (x == y) return 0;
+		x = get(x), y = get(y); 
+        if(x==y){
+            if(rollback) mod.pb(array<int,4>{bigg,bigg,bigg,bigg});
+            return 0;
+        }
 		if (e[x] > e[y]) swap(x,y);
+        if(rollback) mod.pb(array<int,4>{x,y,e[x],e[y]});
 		e[x] += e[y]; e[y] = x; return 1;
 	}
+    void setrb(){rollback=1; DSU();}
+    void rb(){
+        auto a = mod.back(); mod.pop_back();
+		if (a[0] != bigg) e[a[0]] = a[2], e[a[1]] = a[3];
+    }
 	int compnum(){ //return the number of components
 		int ret=1;
 		F0R(i,sz(e)-1) if(get(i)!=get(i+1)) ret++;
 		return ret;
 	}
 }; 
-
-struct DSUrb {
-	vi e;
-	void init(int n) { e = vi(n,-1); }
-	int get(int x) { return e[x] < 0 ? x : get(e[x]); } 
-	bool sameSet(int a, int b) { return get(a) == get(b); }
-	int size(int x) { return -e[get(x)]; }
-	vector<array<int,4>> mod;
-	bool unite(int x, int y) { // union-by-rank
-		x = get(x), y = get(y); 
-		if (x == y) {
-			mod.pb({MOD,MOD,MOD,MOD});
-			return 0;
-		}
-		if (e[x] > e[y]) swap(x,y);
-		mod.pb({x,y,e[x],e[y]});
-		e[x] += e[y]; e[y] = x;
-		return 1;
-	}
-	void rollback() {
-		auto a = mod.bk; mod.pop_back();
-		if (a[0] != MOD) e[a[0]] = a[2], e[a[1]] = a[3];
-	}
-};
 
 template <class T, int SZ> struct Dijk{ //class T is the type of weight being used, works in O(E log V)
 	vector<pair<T,int> > adj[SZ]; T dist[SZ]; bool vis[SZ];
