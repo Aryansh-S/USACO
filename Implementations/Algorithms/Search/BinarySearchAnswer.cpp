55242ce8b2c68921d05c71548daedac5b2df8714
Aryansh-S <63766132+Aryansh-S@users.noreply.github.com>
2020-05-07 16:10:22 -0700

    Update BinarySearchAnswer.cpp

--- Implementations/Algorithms/Search/BinarySearchAnswer.cpp
+++ Implementations/Algorithms/Search/BinarySearchAnswer.cpp
@@ -2,26 +2,21 @@
 using namespace std;
 //level: silver 
 
-//objective: given a value of n specifying a discrete search space [0,n) for a variable k, search for optimal k that works
-
-int n,ans;
-
-int checkifworks(int k){
-  // return 1 if k works, -1 if k too small, 0 if k too big (based on some monotonic conditions)
+bool works(int m){
+ //check if works
 }
 
 int main(){
-  cin >> n; 
-  //check for k from 0 to n-1
-  int l=0,r=n-1;
-  while(l<=r){
-    int m=l+(r-l)/2;
-    if(check(m)==1){ans=m; break;}
-    else if(check(m)==-1 && check(m+1)==0) {ans=m; break;}
-    else if(check(m)==0 && check(m+1)==-1) {ans=m+1; break;}
-    else if(check(m)==-1) l=m+1;
-    else r=m-1;
-  }
-  cout << ans << "\n";
-  return 0;
+  int lo = 0; int hi = N;
+  int ans = 0;
+  while(lo <= hi){
+    int mid = lo+(hi-lo)/2;
+    if(works(mid)){
+      ans = mid;
+      hi = mid-1;
+    }
+    else{
+      lo = mid+1;
+    }
+  } return 0;
 }
