d3b49649f9d6e5bf01daccd2ee5391367c9999b2
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-05-25 16:54:24 -0700

    Update Fraction.cpp

--- Implementations/Algorithms/Fraction.cpp
+++ Implementations/Algorithms/Fraction.cpp
@@ -3,6 +3,7 @@ struct frac{
   void simp(){
     int gcd = __gcd(num,den); num/=gcd, den/=gcd;
   }
+  void rec(){ swap(num,den); }
   bool operator==(frac f){return num * f.den == den * f.num;}
   frac operator+(frac f){
     frac ret = {num * f.den + den * f.num, den * f.den}; ret.simp(); return ret;
