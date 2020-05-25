6404fedde16f5142fa0478aabea9f3fdb84b567a
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-05-25 16:48:26 -0700

    Create Fraction.cpp

---
+++ Implementations/Algorithms/Fraction.cpp
@@ -0,0 +1,20 @@
+struct frac{
+  int num = 0, den = 1; 
+  frac(int n = 0, int d = 1){num = n, den = d;}
+  void simp(){
+    int gcd = __gcd(num,den); num/=gcd, den/=gcd;
+  }
+  bool operator==(frac f){return num * f.den == den * f.num;}
+  frac operator+(frac f){
+    frac ret = {num * f.den + den * f.num, den * f.den}; ret.simp(); return ret;
+  }
+  frac operator-(frac f){
+    frac ret = {num * f.den - den * f.num, den * f.den}; ret.simp(); return ret; 
+  }
+  frac operator*(frac f){
+    frac ret = {num * f.num, den * f.den}; ret.simp(); return ret; 
+  }
+  frac operator/(frac f){
+    frac ret = {num * f.den, den * f.num}; ret.simp(); return ret;
+  }
+};
