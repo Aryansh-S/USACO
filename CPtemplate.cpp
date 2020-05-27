e21d9cef14649c473fffb6a7e43107e24e174bbb
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-05-26 21:25:11 -0700

    Update CPtemplate.cpp

--- CPtemplate.cpp
+++ CPtemplate.cpp
@@ -79,6 +79,23 @@ namespace template_lib {
 	
 	namespace grid_macros {
 		const int xd[4] = {0,1,0,-1}, yd[4] = {1,0,-1,0}; 
+		template<class T> struct bd{ //bdi for int
+			  T v = 0; int l = 0, r = 1;
+			  bd(int _v, int _l, int _r){l=_l,r=_r,v=_v;}
+			  operator int(){return v;}
+			  bd<T> operator++(){
+			    bd<T> tmp(v,l,r); tmp.v = v + 1 > r ? r : ++v; return tmp; 
+			  }
+			  bd<T> operator--(){
+			    bd<T> tmp(v,l,r); tmp.v = v - 1 < l ? l : --v; return tmp;
+			  }
+			  bd<T> operator+(int d){
+			    bd<T> tmp(v,l,r); (d > 0) ? (tmp.v = v + d > r ? r : v + d) : (tmp.v = v + d < l ? l : v + d);
+			    return tmp; 
+			  }
+			  bd<T> operator-(int d){return operator+(-1*d);}
+		};
+		using bdi = bd<int>;
 	}
 	using namespace grid_macros;
 
