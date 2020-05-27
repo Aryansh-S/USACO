588a33d9ad88a9b169962ddf84ebe4d57f1f674d
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-05-26 23:48:32 -0700

    Update algotemplate.cpp

--- algotemplate.cpp
+++ algotemplate.cpp
@@ -6,65 +6,69 @@
  */
 
 #include <bits/stdc++.h> 
-using namespace std; //STL 
+using namespace std;  
 
-/* Policy Based Data Structures (GCC)
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/rope>
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
-template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
-*/
+namespace ignore_this_macro_stuff {
+	/* 
+		Policy Based Data Structures (GCC)
+		#include <ext/pb_ds/assoc_container.hpp>
+		#include <ext/pb_ds/tree_policy.hpp>
+		#include <ext/rope>
+		using namespace __gnu_pbds;
+		using namespace __gnu_cxx;
+		template <class T> using OST = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+	*/
+	typedef long long ll;
+	typedef pair<int,int> pii;
+	typedef pair<ll,ll> pll;
+	typedef vector<int> vi;
+	template<class T> using minpq = priority_queue<T, vector<T>, greater<T> >;
+	template<class T> using maxpq = priority_queue<T>;
+	#define pb push_back
+	#define ins insert
+	#define mp make_pair
+	#define f first
+	#define s second
+	#define sz(x) (int)(x).size()
+	#define rsz resize
+	#define lb lower_bound
+	#define ub upper_bound
+	#define all(x) begin(x),end(x)
+	#define trav(a,x) for(auto&(a):(x))
+	#define FOR(i,a,b) for(__typeof(b)i=(a)-((a)>(b));i!=(b)-((a)>(b));i+=((a)>(b))?-1:1) //go from i=lb to i=ub-1
+	#define F0R(i,b) FOR(i,0,b)
+	#define FOR_(i,a,b,d) for(__typeof(b)i=(a)-((a)>(b));(a>b)?(i>b-1):(i<b);i+=(((a)>(b))?-1:1)*abs(d)) //go from i=lb to i=ub-1 by size d increments
+	#define F0R_(i,b,d) FOR_(i,0,b,d)
+	#define R0F(i,n) FOR(i,n,0)
+	#define R0F_(i,b,d) FOR_(i,b,0,d)
+	#define F1R(i,b) FOR(i,1,b+1)
+	#define R1F(i,b) FOR(i,b+1,1)
+	#define F1R_(i,b,d) FOR_(i,1,b+1,d)
+	#define R1F_(i,b,d) FOR_(i,b+1,1,d)
+	#define what(x) out((#x),"is",(x))
+	#define big (int)1e5
+	#define bigg (int)(1e9+7)
+	#define INF 0x3f3f3f3f
+	#define INFLL 0x3f3f3f3f3f3f3f3f
+	#define EPS 1e-8
+	#define ckmin(a,b) a=min(a,b)
+	#define ckmax(a,b) a=max(a,b)
+	void in(){} void outln(){} void out_(){cout << "\n";} //for fast, easy IO
+	template<typename T, typename...Types> void in(T& var1, Types&...var2){cin >> var1; in(var2...);}
+	template<typename T> void out(T var1){cout << var1 << "\n";}
+	template<typename T, typename...Types> void out(T var1, Types...var2){cout << var1 << " "; out(var2...);}
+	template<typename T, typename...Types> void outln(T var1, Types...var2){cout << var1 << "\n"; outln(var2...);}
+	template<typename T> void out_(T var1){cout << var1 << " ";}
+	bool comps(const pii&p1, const pii&p2){return (p1.s!=p2.s)?(p1.s<p2.s):(p1.f<p2.f);} //sort and search with special pair conditions & compare int-pair
+	struct intpairf{bool operator()(const pii&a, const int&b){return(a.f!=b)?(a.f<b):(a.s<b);}
+	bool operator()(const int&a, const pii&b){return(a!=b.f)?(a<b.f):(a<b.s);}};
+	struct intpairs{bool operator()(const pii&a, const int&b){return(a.s!=b)?(a.s<b):(a.f<b);}
+	bool operator()(const int&a, const pii&b){return(a!=b.s)?(a<b.s):(a<b.f);}};
+}
 
-typedef long long ll;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
-typedef vector<int> vi;
-template<class T> using minpq = priority_queue<T, vector<T>, greater<T> >;
-template<class T> using maxpq = priority_queue<T>;
-#define pb push_back
-#define ins insert
-#define mp make_pair
-#define f first
-#define s second
-#define sz(x) (int)(x).size()
-#define rsz resize
-#define lb lower_bound
-#define ub upper_bound
-#define all(x) begin(x),end(x)
-#define trav(a,x) for(auto&(a):(x))
-#define FOR(i,a,b) for(__typeof(b)i=(a)-((a)>(b));i!=(b)-((a)>(b));i+=((a)>(b))?-1:1) //go from i=lb to i=ub-1
-#define F0R(i,b) FOR(i,0,b)
-#define FOR_(i,a,b,d) for(__typeof(b)i=(a)-((a)>(b));(a>b)?(i>b-1):(i<b);i+=(((a)>(b))?-1:1)*abs(d)) //go from i=lb to i=ub-1 by size d increments
-#define F0R_(i,b,d) FOR_(i,0,b,d)
-#define R0F(i,n) FOR(i,n,0)
-#define R0F_(i,b,d) FOR_(i,b,0,d)
-#define F1R(i,b) FOR(i,1,b+1)
-#define R1F(i,b) FOR(i,b+1,1)
-#define F1R_(i,b,d) FOR_(i,1,b+1,d)
-#define R1F_(i,b,d) FOR_(i,b+1,1,d)
-#define what(x) out((#x),"is",(x))
-#define big (int)1e5
-#define bigg (int)(1e9+7)
-#define INF 0x3f3f3f3f
-#define INFLL 0x3f3f3f3f3f3f3f3f
-#define EPS 1e-8
-#define ckmin(a,b) a=min(a,b)
-#define ckmax(a,b) a=max(a,b)
+using namespace ignore_this_macro_stuff;
 
-void in(){} void outln(){} void out_(){cout << "\n";} //for fast, easy IO
-template<typename T, typename...Types> void in(T& var1, Types&...var2){cin >> var1; in(var2...);}
-template<typename T> void out(T var1){cout << var1 << "\n";}
-template<typename T, typename...Types> void out(T var1, Types...var2){cout << var1 << " "; out(var2...);}
-template<typename T, typename...Types> void outln(T var1, Types...var2){cout << var1 << "\n"; outln(var2...);}
-template<typename T> void out_(T var1){cout << var1 << " ";}
-
-bool comps(const pii&p1, const pii&p2){return (p1.s!=p2.s)?(p1.s<p2.s):(p1.f<p2.f);} //sort and search with special pair conditions & compare int-pair
-struct intpairf{bool operator()(const pii&a, const int&b){return(a.f!=b)?(a.f<b):(a.s<b);}
-bool operator()(const int&a, const pii&b){return(a!=b.f)?(a<b.f):(a<b.s);}};
-struct intpairs{bool operator()(const pii&a, const int&b){return(a.s!=b)?(a.s<b):(a.f<b);}
-bool operator()(const int&a, const pii&b){return(a!=b.s)?(a<b.s):(a<b.f);}};
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 struct radix{const int bit;radix(int off):bit(off){}bool operator()(int val)const{return (bit==31)?(val<0):(!(val&(1<<bit)));}}; //concise linear radix sort
 void rsort(int*l,int*r,int msb=31){
