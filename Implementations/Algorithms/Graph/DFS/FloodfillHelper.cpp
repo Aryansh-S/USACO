c70568e7f5e90270544e6045d95f378f9bf2af77
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-05-26 21:15:17 -0700

    Update FloodfillHelper.cpp

--- Implementations/Algorithms/Graph/DFS/FloodfillHelper.cpp
+++ Implementations/Algorithms/Graph/DFS/FloodfillHelper.cpp
@@ -1,21 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int xd[4] = {0,1,0,-1}, yd[4] = {1,0,-1,0}; 
+
 //bounded type for simple floodfill
 
 template<class T> struct bd{ 
   T v = 0; int l = 0, r = 1;
-  bd(int _l, int _r, int _v){l=_l,r=_r,v=_v;}
+  bd(int _v, int _l, int _r){l=_l,r=_r,v=_v;}
   operator int(){return v;}
   bd<T> operator++(){
-    bd<T> tmp(l,r,v); tmp.v = v + 1 > r ? r : ++v; return tmp; 
+    bd<T> tmp(v,l,r); tmp.v = v + 1 > r ? r : ++v; return tmp; 
   }
   bd<T> operator--(){
-    bd<T> tmp(l,r,v); tmp.v = v - 1 < l ? l : --v; return tmp;
+    bd<T> tmp(v,l,r); tmp.v = v - 1 < l ? l : --v; return tmp;
   }
   bd<T> operator+(int d){
-    bd<T> tmp; d ? (tmp.v = v + d > r ? r : v + d) : (tmp.v = v + d < l ? l : v + d);
-    return 0; 
+    bd<T> tmp(v,l,r); (d > 0) ? (tmp.v = v + d > r ? r : v + d) : (tmp.v = v + d < l ? l : v + d);
+    return tmp; 
   }
   bd<T> operator-(int d){return operator+(-1*d);}
 };
