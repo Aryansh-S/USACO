9b0894d406567e4ae167c5aed4fef5e9548c3fff
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-02 14:33:55 -0700

    Update CPtemplate.cpp

--- CPtemplate.cpp
+++ CPtemplate.cpp
@@ -15,9 +15,11 @@ namespace aryansh {
 		template<class T> using minpq = priority_queue<T, vector<T>, greater<T> >;
 		template<class T> using maxpq = priority_queue<T>;
 		template<class T> using OST = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>; 
+		template<class T> using M_OST = tree<T, null_type, less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
 		template<class T, class U> using OSM = tree<T, U, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
-		template<class T> using hset = gp_hash_table<T, null_type, hash<T>>; 
-		template<class T, class U> using hmap = gp_hash_table<T, U, hash<T>>;
+		template<class T, class U> using M_OSM = tree<T, U, less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+		template<class T> using HSET = gp_hash_table<T, null_type, hash<T>>; 
+		template<class T, class U> using HMAP = gp_hash_table<T, U, hash<T>>;
 	}
 	using namespace type_macros; 
 
