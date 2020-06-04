ddfe2771cc7a19854ba6a716e21c55056bec09a7
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-04 14:53:34 -0700

    Update algos.cpp

--- algos.cpp
+++ algos.cpp
@@ -408,6 +408,30 @@ template<class T> struct SEG { // comb(ID,b) = b, 0-indexing works, any associat
 	}
 };
 
+template<class T> struct SEGRP { //range update, point query
+	//comb must work independent of order, otherwise use lazy
+	const T ID = 1; T comb(T a, T b) { return a * b; } 
+	int n; vector<T> seg;
+	void init(int _n) { n = _n; seg.assign(2*n, ID); } 
+    	void init(vector<T>& v) { init(v.size()); copy(begin(v), end(v), begin(seg) + n); build(); }
+    	void init(T* a, int _n) { init(_n); copy(a, a + n, begin(seg) + n); build(); }
+    	void build() { 
+        	for (int i = 1; i < n; ++i) 
+            		seg[2*i] = comb(seg[2*i],seg[i]), seg[2*i + 1] = comb(seg[2*i + 1],seg[i]), seg[i] = ID;
+    	}
+	T qry(int p) { // qry val at position p
+        	T res = ID; 
+		    for (p += n; p; p /= 2) res = comb(res,seg[p]); 
+        	return res;
+    	}
+	void upd(int l, int r, T val) {	// upd by comb on interval [l, r]
+		for (l += n, r += n+1; l < r; l /= 2, r /= 2) {
+            		if (l&1) seg[l] = comb(seg[l],val), ++l;
+            		if (r&1) seg[r-1] = comb(seg[r-1],val), --r;
+        	} 
+	}
+};
+
 template <class T, int ...Ns> struct BIT{ //UPD only adds!!
 	//general Fenwick BIT struct with range sum queries and point updates for D dimensions
 	T val = 0; void upd(T v) {val += v;} //map<int,BIT> preferable to BIT array
