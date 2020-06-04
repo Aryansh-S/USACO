340d1a09a9ad270c65f8a3e5d0c853e24bfba214
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-03 18:19:30 -0700

    Create iterativesegtree.cpp

---
+++ Implementations/Algorithms/Query DS/iterativesegtree.cpp
@@ -0,0 +1,18 @@
+template<class T> struct SEG { // comb(ID,b) = b, 1-index
+	const T ID = 0; T comb(T a, T b) { return a+b; } 
+	int n; vector<T> seg;
+	void init(int _n) { n = _n; seg.assign(2*n,ID); }
+	void pull(int p) { seg[p] = comb(seg[2*p],seg[2*p+1]); }
+	void upd(int p, T val) { // set val at position p
+		seg[p += n] = val; for (p /= 2; p; p /= 2) pull(p); }
+	T query(int l, int r) {	// sum on interval [l, r]
+		T ra = ID, rb = ID; 
+		for (l += n, r += n+1; l < r; l /= 2, r /= 2) {
+			if (l&1) ra = comb(ra,seg[l++]);
+			if (r&1) rb = comb(seg[--r],rb);
+		}
+		return comb(ra,rb);
+	}
+};
+
+
