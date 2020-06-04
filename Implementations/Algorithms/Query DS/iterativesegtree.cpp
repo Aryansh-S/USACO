f582d4eb028065213534b75d8597494c41404b1f
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-04 01:14:43 -0700

    Update iterativesegtree.cpp

--- Implementations/Algorithms/Query DS/iterativesegtree.cpp
+++ Implementations/Algorithms/Query DS/iterativesegtree.cpp
@@ -1,12 +1,16 @@
 //Iterative Generic Segment Tree
 
-template<class T> struct SEG { // comb(ID,b) = b, 1-index
-	const T ID = 0; T comb(T a, T b) { return a+b; } 
+template<class T> struct SEG { // comb(ID,b) = b, 0-indexing works
+	const T ID = -INF; T comb(T a, T b) { return max(a,b); } 
 	int n; vector<T> seg;
-	void init(int _n) { n = _n; seg.assign(2*n,ID); }
+	void init(int _n) { n = _n; seg.assign(2*n, ID); } 
+    	void init(vector<T>& v) { init(v.size()); copy(begin(v), end(v), begin(seg) + n); build(); }
+    	void init(T* a, int _n) { init(_n); copy(a, a + n, begin(seg) + n); build(); }
+    	void build() { for(int i = n - 1; i > 0; --i) pull(i); }
 	void pull(int p) { seg[p] = comb(seg[2*p],seg[2*p+1]); }
 	void upd(int p, T val) { // set val at position p
-		seg[p += n] = val; for (p /= 2; p; p /= 2) pull(p); }
+		seg[p += n] = val; for (p /= 2; p; p /= 2) pull(p); 
+    	}
 	T query(int l, int r) {	// sum on interval [l, r]
 		T ra = ID, rb = ID; 
 		for (l += n, r += n+1; l < r; l /= 2, r /= 2) {
