41c77385186d0faf34ea5d6a463e17a15528f0dd
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-04 14:52:28 -0700

    Update rng_upd_pt_qry.cpp

--- Implementations/Algorithms/Query DS/Easy_Segtrees/rng_upd_pt_qry.cpp
+++ Implementations/Algorithms/Query DS/Easy_Segtrees/rng_upd_pt_qry.cpp
@@ -5,7 +5,7 @@
 
 template<class T> struct SEGRP { //range update, point query
 	//comb must work independent of order, otherwise use lazy
-	const T ID = 0; T comb(T a, T b) { return a + b; } 
+	const T ID = 1; T comb(T a, T b) { return a * b; } 
 	int n; vector<T> seg;
 	void init(int _n) { n = _n; seg.assign(2*n, ID); } 
     	void init(vector<T>& v) { init(v.size()); copy(begin(v), end(v), begin(seg) + n); build(); }
@@ -16,12 +16,12 @@ template<class T> struct SEGRP { //range update, point query
     	}
 	T qry(int p) { // qry val at position p
         	T res = ID; 
-		for (p += n; p; p /= 2) res = comb(res,seg[p]); 
+		    for (p += n; p; p /= 2) res = comb(res,seg[p]); 
         	return res;
     	}
 	void upd(int l, int r, T val) {	// upd by comb on interval [l, r]
 		for (l += n, r += n+1; l < r; l /= 2, r /= 2) {
-            		if (l&1) seg[l] = comb(seg[l+1],val), ++l;
+            		if (l&1) seg[l] = comb(seg[l],val), ++l;
             		if (r&1) seg[r-1] = comb(seg[r-1],val), --r;
         	} 
 	}
