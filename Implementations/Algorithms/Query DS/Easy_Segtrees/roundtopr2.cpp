2488d86ffbedc7379204fd7c98512d277bd6223e
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-04 19:31:30 -0700

    Update roundtopr2.cpp

--- Implementations/Algorithms/Query DS/Easy_Segtrees/roundtopr2.cpp
+++ Implementations/Algorithms/Query DS/Easy_Segtrees/roundtopr2.cpp
@@ -1,7 +1,7 @@
 //helper function if needed to balance the tree for binary search, etc.
 //use to quickly & efficiently balance n for use in init iterative segtree
 
-ll POW(ll a, ll b) { //log
+ll POW(ll a, ll b) { //log (from template)
 	ll res = 1; while(b) res *= (b & 1)? a : 1, a*=a, b>>=1;
 	return res;
 }
