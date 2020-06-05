b58db03597367b31a83673d47003b341509bfef0
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-04 19:19:53 -0700

    Create roundtopr2.cpp

---
+++ Implementations/Algorithms/Query DS/Easy_Segtrees/roundtopr2.cpp
@@ -0,0 +1,11 @@
+//helper function if needed to balance the tree for binary search, etc.
+
+int upper_power_of_two(int v) {
+    --v;
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+    return ++v;
+}
