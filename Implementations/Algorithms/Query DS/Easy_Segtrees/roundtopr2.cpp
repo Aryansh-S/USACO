1b6707fb247a593a0797d399b7c42440d878ab28
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-04 20:50:53 -0700

    Update roundtopr2.cpp

--- Implementations/Algorithms/Query DS/Easy_Segtrees/roundtopr2.cpp
+++ Implementations/Algorithms/Query DS/Easy_Segtrees/roundtopr2.cpp
@@ -1,12 +1,4 @@
-//helper function if needed to balance the tree for binary search, etc.
-//use to quickly & efficiently balance n for use in init iterative segtree
-
-ll POW(ll a, ll b) { //log (from template)
-	ll res = 1; while(b) res *= (b & 1)? a : 1, a*=a, b>>=1;
-	return res;
-}
-
-int nxtp2(int v) { //next power 2
-    int ret = POW(2,31-__builtin_clz(v)); 
-    return v > ret ? ret<<1 : ret;
+int nxtpw2(int v){
+	if((bool)v & !(v & (v - 1))) return v; 
+	int x = 1; while(x < v) x<<1; return x; 
 }
