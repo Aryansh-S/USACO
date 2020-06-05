d81c470ab7a23e853523a3efc58d779508ef9c20
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-04 18:02:22 -0700

    Update BITMadeEasy.cpp

--- Implementations/Algorithms/Query DS/BITMadeEasy.cpp
+++ Implementations/Algorithms/Query DS/BITMadeEasy.cpp
@@ -3,13 +3,18 @@ using namespace std;
 //BIT is very easy to implement!
 
 struct BIT {
+    //init things
     int n; vector<int> bit; 
     void init(int _n){n = _n; bit.assign(n);}
     void init(int* a, int _n){init(_n); move(a,a+n,begin(bit));}
     void init(vector<int>& v){init(v.size()); move(begin(v),end(v),begin(bit));}
+    
+    //actual bit
     void upd(int x, int v){for(;x<=100;x+=x&-x) bit[x]+=v;}
     int sum(int r){int res=0; for(;r;r-=r&-r) res+=bit[r]; return res;}
     int sum(int l, int r){return sum(r)-sum(l-1);}
+    
+    //extension
     void upd_a(int x, int v){upd(x,val-sum(x,x));} //assignment update example
 } tree; 
 
