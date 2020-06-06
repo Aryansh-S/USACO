41ee88c237bf9284fc51429113ae24339769fd57
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-06 15:07:44 -0700

    Update CPtemplate.cpp

--- CPtemplate.cpp
+++ CPtemplate.cpp
@@ -5,16 +5,20 @@
 using namespace std; using namespace __gnu_pbds; //cmd + shift + r
 
 namespace aryansh { 
+	
 	namespace type_macros {
-		using ll = long long; using db = double; using ld = long double; using ull = unsigned long long; 
-		using LL = __uint128_t; using str = string; using pii = pair<int,int>; using pll = pair<ll,ll>;
-		using vi = vector<int>; 
+		using ll = long long; using db = double; using ld = long double; using str = string; using pii = pair<int,int>;
+		using pll = pair<ll,ll>; using vi = vector<int>; 
+		
 		template<class T> using minpq = priority_queue<T, vector<T>, greater<T> >;
 		template<class T> using maxpq = priority_queue<T>;
+		
 		template<class T> using OST = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>; 
 		template<class T, class U> using OSM = tree<T, U, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+		
 		template<class T> using hset = gp_hash_table<T, null_type, hash<T>>; //don't use unordered types 
 		template<class T, class U> using hmap = gp_hash_table<T, U, hash<T>>; 
+		
 		template<class T> inline str type_name() { //get type as string
 			typedef typename remove_reference<T>::type TR;
 			unique_ptr<char, void(*)(void*)> own(abi::__cxa_demangle(typeid(TR).name(), nullptr,nullptr,nullptr),free); 
@@ -31,6 +35,7 @@ namespace aryansh {
 	namespace func_macros {
 		#define fbo find_by_order //which element has x ones < it, or end
 		#define ofk order_of_key //how many elements are/would be < it, or end
+		
 		#define pb push_back
 		#define pf push_front
 		#define ppb pop_back
@@ -40,16 +45,21 @@ namespace aryansh {
 		#define ins insert
 		#define emp emplace //faster than ins, not for pbds
 		#define ers erase
+		
 		#define mp make_pair
 		#define f first
 		#define s second
+		
+		#define sq(x) (x)*(x)
+		
 		#define empt(x) (x).empty()
 		#define sz(x) (empt(x) ? 0 : (int)(x).size())
-		#define sq(x) (x)*(x)
 		#define rsz resize
+		
 		#define lb lower_bound //first el in [left_it,right_it) >= val
 		#define ub upper_bound //first el in [left_it,right_it) > val
 		#define has(x,y) ((x).find(y)!=end(x)) //for nonlinear containers
+		
 		#define all(x) begin(x),end(x)
 		#define rall(x) end(x),begin(x)
 	}
@@ -57,17 +67,20 @@ namespace aryansh {
 
 	namespace loop_macros {
 		#define trav(a,x) for(auto&(a):(x))
+		
 		#define FOR(i,a,b) for(auto i=decltype(b){(a)-((a)>(b))};i!=(b)-((a)>(b));((a)>(b))?--i:++i) 
 			//[lb,ub-1]
-		#define F0R(i,b) FOR(i,0,b)
 		#define FOR_(i,a,b,d) for(auto i=decltype(b){(a)-((a)>(b))};(a>b)?(i>b-1):(i<b);i+=(((a)>(b))?-1:1)*abs(d))
 			//[lb,ub-1] by d
+		
+		#define F0R(i,b) FOR(i,0,b)
 		#define F0R_(i,b,d) FOR_(i,0,b,d)
+		#define F1R(i,b) FOR(i,1,b+1)
+		#define F1R_(i,b,d) FOR_(i,1,b+1,d)
+		
 		#define R0F(i,n) FOR(i,n,0)
 		#define R0F_(i,b,d) FOR_(i,b,0,d)
-		#define F1R(i,b) FOR(i,1,b+1)
 		#define R1F(i,b) FOR(i,b+1,1)
-		#define F1R_(i,b,d) FOR_(i,1,b+1,d)
 		#define R1F_(i,b,d) FOR_(i,b+1,1,d)
 	}
 	using namespace loop_macros;
@@ -75,9 +88,12 @@ namespace aryansh {
 	namespace const_macros {
 		#define big (int)1e5
 		#define bigg (int)(1e9+7)
+		
 		#define MOD (int)(1e9+7)
+		
 		#define INF 0x3f3f3f3f
 		#define INFLL 0x3f3f3f3f3f3f3f3f
+		
 		#define EPS 1e-8
 	}
 	using namespace const_macros;
@@ -85,12 +101,15 @@ namespace aryansh {
 	namespace math_macros {
 		#define ckmin(a,b) a=min(a,b)
 		#define ckmax(a,b) a=max(a,b)
+		
 		#define gcd(a,b) ((!(a) || !(b)) ? (max(abs(a),abs(b))) : (__gcd((a),(b))))
 		#define lcm(a,b) ((!(a) || !(b)) ? 0 : ((a)*(b))/gcd((a),(b)))
+		
 		ll POW(ll a, ll b) { //log
 			ll res = 1; while(b) res *= (b & 1)? a : 1, a*=a, b>>=1;
 			return res;
 		}
+		
 		struct mi { //modular int
 			typedef decay<decltype(MOD)>::type T;
 			T val; 
@@ -138,6 +157,7 @@ namespace aryansh {
 	
 	namespace grid_macros {
 		const int xd[4] = {0,1,0,-1}, yd[4] = {1,0,-1,0}; 
+		
 		template<class T> struct bd{ //bdi for inclusive bounded int
 			  T v = 0, l = 0, r = 1;
 			  bd(T _v, T _l, T _r){l=_l,r=_r,v=_v; if(v < l) v = l; if(v > r) v = r;}
@@ -173,30 +193,38 @@ namespace aryansh {
 
 	namespace io_macros {
 		inline void in(){} inline void outln(){} inline void out_(){} //for fast, easy IO
+		
 		template<typename T, typename...Types> inline void in(T& var1, Types&...var2){cin >> var1; in(var2...);}
-		template<typename T> inline void out(T var1){cout << var1 << "\n";}
-		template<typename T, typename...Types> inline void out(T var1, Types...var2){cout << var1 << " "; out(var2...);}
-		template<typename T, typename...Types> inline void outln(T var1, Types...var2)
-			{cout << var1 << "\n"; outln(var2...);}
-		template<typename T,typename...Types> inline void out_(T var1, Types...var2){cout << var1 << " "; out_(var2...);}
-		template<class T1, class T2> inline void in(pair<T1,T2>&pt){in(pt.f,pt.s);} 
-		template<class T1, class T2> inline void out(pair<T1,T2> pt){out(pt.f,pt.s);}
-		template<class T1, class T2> inline void out_(pair<T1,T2> pt){out_(pt.f,pt.s);}
+		template<class T1, class T2> inline void in(pair<T1,T2>&pt){in(pt.f,pt.s);}
 		template<class T> inline void in(vector<T>&v,int sz){F0R(i,sz){T x; in(x); v.pb(x);}}
 		template<class T> inline void in(T*a,int sz){F0R(i,sz) in(a[i]);}
+		
+		template<typename T> inline void out(T var1){cout << var1 << "\n";}
+		template<typename T, typename...Types> inline void out(T var1, Types...var2){cout << var1 << " "; out(var2...);}
+		template<class T1, class T2> inline void out(pair<T1,T2> pt){out(pt.f,pt.s);}
 		template<class T> inline void out(vector<T> v){trav(t,v) out(t);}
 		template<class T> inline void out(vector<T> v,int sz){trav(t,v) out(t);}
+		template<class T> inline void out(T*a,int sz){F0R(i,sz) out(a[i]);}
+	
+		template<typename T,typename...Types> inline void out_(T var1, Types...var2){cout << var1 << " "; out_(var2...);}
 		template<class T> inline void out_(vector<T> v){trav(t,v) out_(t);} //might need to int cast size
 		template<class T> inline void out_(vector<T> v,int sz){trav(t,v) out_(t);}
-		template<class T> inline void out(T*a,int sz){F0R(i,sz) out(a[i]);}
 		template<class T> inline void out_(T*a,int sz){F0R(i,sz) out_(a[i]);}
+		template<class T1, class T2> inline void out_(pair<T1,T2> pt){out_(pt.f,pt.s);}
+		
+		template<typename T, typename...Types> inline void outln(T var1, Types...var2)
+			{cout << var1 << "\n"; outln(var2...);}	
+		
+		#define NL cout << "\n"
+		#define what(x) out_((#x));out_("is");out_(x);NL
 		#define PRES(d) cout.precision(d);cout.setf(ios::fixed,ios::floatfield)
+		
 		#define MEM(a,v) \
 		if((!(v) || !((v)+1) || !((v)-INF) || !((v)+INF))) {\
 		    (memset(&(a)[0],(v),sizeof(a)));}else {trav(k,(a)) {k=v;}}static_assert(1, "")
-		#define NL cout << "\n"
-		#define what(x) out_((#x));out_("is");out_(x);NL
+		
 		#define type(x) type_name<decltype(x)>()
+		
 		#define IO(PNAME) \
 		if(fopen(PNAME ".in","r")){freopen(PNAME ".in","r",stdin);freopen(PNAME ".out","w",stdout);} \
 		cin.tie(0)->sync_with_stdio(0)
