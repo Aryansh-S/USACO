f47e0e72d7a2e35ca68c2b8e88dd605fe4e7f58c
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-09 14:24:05 -0700

    Update rng_upd_pt_qry.cpp

--- Implementations/Algorithms/Query DS/Easy_Segtrees/rng_upd_pt_qry.cpp
+++ Implementations/Algorithms/Query DS/Easy_Segtrees/rng_upd_pt_qry.cpp
@@ -8,8 +8,7 @@ template<class T> struct SEGRP { //range update, point query
 	const T ID = 1; T comb(T a, T b) { return a * b; } 
 	int n; vector<T> seg;
 	void init(int _n) { n = _n; seg.assign(2*n, ID); } 
-    	template<typename it, typename = typename enable_if<is_pointer<it>::value>::type> 
-	void init(it bg, it nd) {init(distance(bg,nd)); move(bg, nd, begin(seg) + n); build();}
+    	template<typename it> void init(it bg, it nd) {init(distance(bg,nd)); move(bg, nd, begin(seg) + n); build();}
     	void build() { 
         	for (int i = 1; i < n; ++i) 
             		seg[2*i] = comb(seg[2*i],seg[i]), seg[2*i + 1] = comb(seg[2*i + 1],seg[i]), seg[i] = ID;
