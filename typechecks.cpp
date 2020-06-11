87b366bf8e06d716c00f9c56c865c20e85d3ab7a
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-10 17:12:17 -0700

    Update typechecks.cpp

--- typechecks.cpp
+++ typechecks.cpp
@@ -24,18 +24,18 @@ template<class T> inline string type_name() {
 #define type(x) type_name<decltype(x)>()
 
 template<class T> class is_iterator { //check if iterator or pointer   
-  static T makeT();
-  typedef void * twoptrs[2];  
-  static twoptrs & test(...); 
-  template<class R> static typename R::iterator_category * test(R); // Iterator
-  template<class R> static void * test(R *); // Pointer
-  public: static const bool value = sizeof(test(makeT())) == sizeof(void *); 
+	static T makeT();
+	typedef void * twoptrs[2];  
+	static twoptrs & test(...); 
+	template<class R> static typename R::iterator_category * test(R); // Iterator
+	template<class R> static void * test(R *); // Pointer
+	public: static const bool value = sizeof(test(makeT())) == sizeof(void *); 
 };
 template<class T> class is_streamable { //check if can be used with cin >>, cout << 
-  template<typename SS, typename TT> static auto test(int)->
-  decltype(declval<SS&>() << declval<TT>(),true_type());
-  template<typename, typename> static auto test(...)->false_type;
-  public: static const bool value = decltype(test<ostream, const T&>(0))::value;
+	template<typename SS, typename TT> static auto test(int)->
+	decltype(declval<SS&>() << declval<TT>(),true_type());
+	template<typename, typename> static auto test(...)->false_type;
+	public: static const bool value = decltype(test<ostream, const T&>(0))::value;
 };
 
 // -- OTHER (POSSIBLY USEFUL) -- //
@@ -65,8 +65,8 @@ using namespace extend_std;
 int a = 0x3f, b[]={1,2,3}; vector<int> c = {1,2,3};
 
 int main() {
-    assert(*begin(a)==a);
-    assert(*(end(a)-1)==a);
-    assert(*begin(b)==b[0]);
-    assert(*(end(c)-1)==c[c.size()-1]);
+	assert(*begin(a)==a);
+	assert(*(end(a)-1)==a);
+	assert(*begin(b)==b[0]);
+	assert(*(end(c)-1)==c[c.size()-1]);
 }
