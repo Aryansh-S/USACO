958e4c2831bdb342218d71f9963a258976f7fd10
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-13 00:00:46 -0700

    Update BITMadeEasy.cpp

--- Implementations/Algorithms/Query DS/BITMadeEasy.cpp
+++ Implementations/Algorithms/Query DS/BITMadeEasy.cpp
@@ -10,16 +10,16 @@ template<class T> struct BIT { //1-indexed!
 
     //init things
     int N; vector<T> bit; 
-    void init(int n){bit.assign(n, ID); N=--n;}
+    void init(int n) { bit.assign(n, ID); N=--n; }
 
     //actual bit
-    void upd(int x, T v){for(;x<=N;x+=x&-x) bit[x]=comb(bit[x],v);}
-    T qry(int r){T res = ID; for(;r;r-=r&-r) res=comb(res,bit[r]); return res;}
-    T qry(int l, int r){return inv_comb(qry(r),qry(l-1));}
+    void upd(int x, T v) { for(; x<=N; x += x & -x) bit[x]=comb(bit[x],v); }
+    T qry(int r) { T res = ID; for(; r; r -= r & -r) res=comb(res,bit[r]); return res; }
+    T qry(int l, int r) { return inv_comb(qry(r),qry(l-1)); }
     
     //extension
-    T get(int x){return qry(x,x);}
-    void put(int x, int v){upd(x,inv_comb(v, get(x)));} //put/assignment update example
+    T get(int x) { return qry(x,x); }
+    void put(int x, int v) { upd(x,inv_comb(v, get(x))); } //put/assignment update example
 };
 
 BIT<int> tree; 
