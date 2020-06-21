54c76569c98aecd86393c90a8469a5ed16ea95d7
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-20 21:06:03 -0700

    Update BigInteger.h

--- Implementations/Algorithms/BigInteger.h
+++ Implementations/Algorithms/BigInteger.h
@@ -1,6 +1,6 @@
 //a convenient, easy-to-use library for C++ when working with very big integers
 //credit: https://sites.google.com/site/indy256/algo_cpp/bigint
-//condensed for ease of use
+//condensed for ease of use & modified
 
 struct bigint {
     static const int base = 1e9;
