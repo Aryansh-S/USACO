54d7096b35e02d57c653140947e5ebcc51cf2a9e
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-22 17:10:17 -0700

    Update seglib.h

--- Implementations/Algorithms/Query DS/Easy_Segtrees/seglib.h
+++ Implementations/Algorithms/Query DS/Easy_Segtrees/seglib.h
@@ -1,4 +1,4 @@
-//Iterative Segment Tree Library
+//Iterative Segment Tree 
 
 template<class T> struct SEG {
     const T ID = 0; T comb(T a, T b) { return a + b; } 
@@ -29,33 +29,36 @@ template<class T> struct SEG {
     }
 };
 
+//Reverse Iterative Segment Tree (Commutative Operations) 
 
-  //Reverse Iterative Generic Segment Tree: Range Update, Point Query (0 - Indexing Allowed), Exactly 2N Memory (Not 4N)
-  //Suggested to use N as power of 2 if doing binary search, order statistics, etc. (perfect binary tree)
-
-  template<class T> struct RSEG { //reverse segtree: range update, point query
-    //comb must work independent of order, otherwise use lazy
-
+template<class T> struct RSEG { 
     const T ID = 1; T comb(T a, T b) { return a * b; } 
 
     int n; vector<T> seg;
-    void init(int _n) { n = _n; seg.assign(2*n, ID); } 
+
+    void init(int n0) { n = n0; seg.assign(2 * n, ID); } 
     template<typename it, typename = typename enable_if<is_iterator<it>::value>::type> 
-    void init(it bg, it nd) {init(distance(bg,nd)); move(bg, nd, begin(seg) + n); build();}
-    void build() { 
-      for (int i = 1; i < n; ++i) 
-      seg[2*i] = comb(seg[2*i],seg[i]), seg[2*i + 1] = comb(seg[2*i + 1],seg[i]), seg[i] = ID;
+    void init(it bg, it nd) {init(distance(bg, nd)); move(bg, nd, begin(seg) + n); build();}
+
+    void build() { for(int i = 1; i < n; ++i) pull(i); }
+
+    inline int lc(int p) { return 2 * p; }
+    inline int rc(int p) { return lc(p) + 1; }
+    void pull(int p) { 
+        seg[lc(p)] = comb(seg[lc(p)], seg[p]), 
+        seg[rc(p)] = comb(seg[rc(p)], seg[p]), 
+        seg[p] = ID; 
     }
-    T qry(int p) { // qry val at position p
+
+    T qry(int p) {
       T res = ID; 
       for (p += n; p; p /= 2) res = comb(res,seg[p]); 
       return res;
     }
-    void upd(int l, int r, T val) { //upd by comb on interval [l, r]
-      for (l += n, r += n+1; l < r; l /= 2, r /= 2) {
-        if (l&1) seg[l] = comb(seg[l],val), ++l;
-        if (r&1) seg[r-1] = comb(seg[r-1],val), --r;
+    void upd(int l, int r, T v) { 
+      for (l += n, r += n + 1; l < r; l /= 2, r /= 2) {
+        if (l & 1) seg[l] = comb(seg[l], v), ++l;
+        if (r & 1) seg[r - 1] = comb(seg[r - 1], v), --r;
       } 
     }
-  };
-    
+};
