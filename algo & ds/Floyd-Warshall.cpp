ea956233db1db84f97b3d3c6c0f5c1e0f7f2df97
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-24 15:48:46 -0700

    Update Floyd-Warshall.cpp

--- algo & ds/Floyd-Warshall.cpp
+++ algo & ds/Floyd-Warshall.cpp
@@ -1,25 +1,11 @@
-#include <bits/stdc++.h>
-using namespace std;
-//level: gold
-
-//objective: use the floyd-warshall graph algorithm to find the shortest distance between all pairs of vertices 
-//advantages: easy to implement, works for negative edges, can be changed to longest path, etc...can detect negative cycles
-//disadvantages: O(N^3) complexity
-
-#define INF 0x3f3f3f3f
-#define ckmin(a,b) a=min(a,b)
-const int sz=1e3;
-int dist[sz][sz], n, m; 
-
-int main(){
-  cin.tie(0)->sync_with_stdio(0);
-  cin >> n >> m; 
-  for(int i=0; i<n; i++) for(int j=0; j<n; j++) dist[i][j]=INF; 
-  for(int i=0; i<m; i++){int x,y,d; cin >> x >> y >> d; x--,y--; dist[x][y]=dist[y][x]=d;}
-  for(int k=0; k<n; k++) for(int i=0; i<n; i++) for(int j=0; j<n; j++){
-    ckmin(dist[i][j],dist[i][k]+dist[k][j]);
+template<class T, int SZ> struct Floyd {
+  T dist[SZ][SZ];
+  Floyd(){F0R(i,SZ) F0R(j,SZ) dist[i][j]=INF;}
+  void add(int a, int b, T w){dist[a][b]=dist[b][a]=w;}
+  void add_(int a, int b, T w){dist[a][b]=w;}
+  void upd(){
+    F0R(k,SZ) F0R(i,SZ) F0R(j,SZ) ckmin(dist[i][j],dist[i][k]+dist[k][j]);
   }
-  for(int i=0; i<n; i++) for(int j=i; j<n; j++) if(i!=j) 
-    cout << "The shortest distance between vertices " << i+1 << " and " << j+1 << " is " << dist[i][j] << "\n"; 
-  return 0;
-}
+  T query(int a, int b){return dist[a][b];}
+  bool negcyc(){F0R(i,SZ) if(dist[i][i]<0) return 1; return 0;}
+};
