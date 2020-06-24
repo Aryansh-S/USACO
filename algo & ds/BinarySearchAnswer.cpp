9cc21b9a1cb8d5e65ca12ec4839891bd2f70c7f8
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-24 16:54:56 -0700

    Update BinarySearchAnswer.cpp

--- algo & ds/BinarySearchAnswer.cpp
+++ algo & ds/BinarySearchAnswer.cpp
@@ -8,14 +8,31 @@ bool works(int m){
  //otherwise won't work
 }
 
-int main(){
-  int lo = 0, hi = N, ans = 0;
-  while(lo <= hi){
+
+#ifdef increasing //from not works to works as var increases (want min val that works in lo...hi)
+
+int main() {
+  int lo = 0, hi = N - 1, ans = lo;
+  while(lo <= hi) {
     int mid = lo + (hi - lo) / 2;
     if(works(mid)) ans = mid, hi = mid - 1;
     else lo = mid + 1;
   } 
 }
 
+#endif
+
+#ifdef decreasing //from works to not works as var increases (want max val that works in lo...hi)
+
+int main() {
+    int lo = 0, hi = N - 1, ans = hi; 
+    while(lo <= hi) {
+        int mid = lo + (hi - lo) / 2; 
+        if(works(mid)) ans = mid, lo = mid + 1
+        else hi = mid - 1; 
+    }
+}
+
+#endif
 
 //footnote: (hi + low) can overflow, so do hi + (hi - lo) / 2 instead of (hi + lo) / 2
