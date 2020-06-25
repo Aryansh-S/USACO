5fce75acb6bbb3c5668efe20b3c481c8f160c5a3
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-24 19:19:14 -0700

    Update BinarySearchAnswer.cpp

--- algo & ds/BinarySearchAnswer.cpp
+++ algo & ds/BinarySearchAnswer.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 //level: silver 
 
-template<class T, typename lamb> T bs(T l, T r, lamb works, bool tp) { 
+template<class T, typename lamb> T bs(T l, T r, lamb works, bool tp, T inval = -1) { 
     //search works() in [l,r], where works() is an inequality bool >= or <= 
     //tp = 0 for find min, tp = 1 for find max
+    //returns inval if invalid
     
     T lo = l, hi = r, ans = tp ? r : l;
     while(lo <= hi) {
@@ -12,7 +13,7 @@ template<class T, typename lamb> T bs(T l, T r, lamb works, bool tp) {
         if(works(mid)) ans = mid, tp ? lo = mid + 1 : hi = mid - 1;
         else tp ? hi = mid - 1 : lo = mid + 1; 
     }
-    return works(ans) ? ans : INT_MAX; 
+    return works(ans) ? ans : inval; 
 }
 
 //footnote: (hi + low) can overflow, so do hi + (hi - lo) / 2 instead of (hi + lo) / 2
