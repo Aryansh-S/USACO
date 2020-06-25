ed278973e75d97fff775db9a7eb8aa7d862c257a
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-24 19:13:34 -0700

    Update BinarySearchAnswer.cpp

--- algo & ds/BinarySearchAnswer.cpp
+++ algo & ds/BinarySearchAnswer.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 //level: silver 
 
-template<class T, typename lamb> T bs(T l, T r, bool tp, lamb works, T inval = -1) { 
+template<class T, typename lamb> T bs(T l, T r, lamb works, bool tp, T inval = -1) { 
     //search works() in [l,r], where works() is an inequality bool >= or <= 
     //tp = 0 for find min, tp = 1 for find max
     //returns inval if invalid
@@ -20,6 +20,6 @@ template<class T, typename lamb> T bs(T l, T r, bool tp, lamb works, T inval = -
 
 
 int main() {
-    assert(bs(1, 10, 0, [](int m){return m * m >= 25}) == 5); 
+    assert(bs(1, 10, [](int m){return m * m >= 25;}, 0) == 5); 
 }
 
