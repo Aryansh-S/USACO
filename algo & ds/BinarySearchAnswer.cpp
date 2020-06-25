4411cb4fd75d355d440231380caa7829de330ef7
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-24 17:11:18 -0700

    Update BinarySearchAnswer.cpp

--- algo & ds/BinarySearchAnswer.cpp
+++ algo & ds/BinarySearchAnswer.cpp
@@ -2,37 +2,20 @@
 using namespace std;
 //level: silver 
 
-bool works(int m){
- //check if works
- //make this an inequality that can be true over a search space monotonically instead of a flat equation 
- //otherwise won't work
+bool works(int m) { 
+    //write a <= or >= equality to return bool
 }
 
-
-#ifdef increasing 
-
-int main() {
-  int lo = 0, hi = N - 1, ans = -1; //want min val works in lo...hi
-  while(lo <= hi) {
-    int mid = lo + (hi - lo) / 2;
-    if(works(mid)) ans = mid, hi = mid - 1;
-    else lo = mid + 1;
-  } 
-}
-
-#endif
-
-#ifdef decreasing 
-
-int main() {
-    int lo = 0, hi = N - 1, ans = -1; //want max val works in lo...hi
+int bs(int l, int r, bool tp) { 
+    //search works() in [l,r]
+    //tp = 0 for find min, tp = 1 for find max
+    int lo = l, hi = r, ans = -1; 
     while(lo <= hi) {
         int mid = lo + (hi - lo) / 2; 
-        if(works(mid)) ans = mid, lo = mid + 1;
-        else hi = mid - 1; 
+        if(works(mid)) ans = mid, tp ? hi = mid - 1 : lo = mid + 1; 
+        else tp ? lo = mid + 1 : hi = mid - 1; 
     }
+    return ans; 
 }
-
-#endif
 
 //footnote: (hi + low) can overflow, so do hi + (hi - lo) / 2 instead of (hi + lo) / 2
