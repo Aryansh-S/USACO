64208bcdf001fd12e0bce1e3f4df3818022f1f2f
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-25 12:29:13 -0700

    Update CPtemplate.cpp

--- CPtemplate.cpp
+++ CPtemplate.cpp
@@ -107,11 +107,11 @@ namespace aryansh {
 			//first el in [left_it,right_it) > val
 				//set::lb/ub, not std::lb/ub
 		
-		template<class T, typename fnc> T bsearch(T l, T r, fnc&& works, bool tp, T inval = -1) { 
+		template<class T, typename fnc> T bsearch(T l, T r, fnc&& works, int tp = 3, T inval = -1) { 
 			//search works() in [l,r], where works() is an inequality bool >= or <= 
 			//tp = 0 for find min, tp = 1 for find max
 			//returns inval if invalid
-			
+			if(tp != 0 && tp != 1) to = works(l); 
 			T lo = l, hi = r, ans = tp ? r : l;
 			while(lo <= hi) {
 				T mid = lo + (hi - lo) / 2; 
