ec7a3e13967625148bab95de0dbfaa1c96d2b539
Aryansh-S <63766132+Aryansh-S@users.noreply.github.com>
2020-06-28 14:06:02 -0700

    f

--- general/USACO/2019/Dec/Gold/milkvisits.cpp
+++ general/USACO/2019/Dec/Gold/milkvisits.cpp
@@ -216,7 +216,7 @@ namespace aryansh {
 		template<typename it, typename = typename enable_if<is_iterator<it>::value>::type> 
 		inline void in(it bg, it nd) 
 			{while(distance(bg,nd)) in(*bg), ++bg;}
-		
+		inline void out(){NL;}
 		template<typename T, typename = typename enable_if<is_streamable<T>::value>::type> 
 		inline void out_(T var1)
 			{cout << var1;}
@@ -275,11 +275,9 @@ using namespace aryansh; auto TICK; //for best results, TICK after input taken
 
 // - - - - - - - - - - - - - - - - - - - - - - - -
 
-template<class T0> struct SEG {
-    using T = vector<T0>;
-    
+template<class T> struct SEG {
     //#define DYNAMIC	
-    const T ID = {}; T comb(T a, T b) { T ret; merge(all(a),all(b),back_inserter(ret)); return ret; }
+    const T ID = -INF; T comb(T a, T b) { return min(a,b); }
     
     int n; 
     
@@ -313,36 +311,138 @@ template<class T0> struct SEG {
 
     inline int lc(int p) { return 2 * p; }
     inline int rc(int p) { return lc(p) + 1; }
-    
     void pull(int p) { seg[p] = comb(get(lc(p)), get(rc(p))); }
-    
-    void upd(int p, T0 v) {
-      seg[p += n] = (T){v};  
-      for(p /= 2; p; p /= 2) pull(p);
+
+    void upd(int p, T v) {
+        seg[p += n] = v; 
+        for(p /= 2; p; p /= 2) pull(p);
     }
-    bool qry(int l, int r, T0 x) { //modified for existence qry
-      bool ret = 0; 
-      for(l += n, r += n + 1; l < r; l /= 2, r /= 2) {
-          if(l & 1) {
-            auto L = get(l++); 
-            ret = ret || binary_search(all(L),x); 
-          }
-          if(r & 1) {
-            auto R = get(--r); 
-            ret = ret || binary_search(all(R),x);
-          }
-      }
-      return ret;
+    T qry(int l, int r) {
+        T ra = ID, rb = ID; 
+        for(l += n, r += n + 1; l < r; l /= 2, r /= 2) {
+            if(l & 1) ra = comb(ra, get(l++)); 
+            if(r & 1) rb = comb(get(--r), rb);
+        }
+        return comb(ra, rb); 
     }
 };
 
-SEG<int> s; vector<vi> v{{1},{2},{3}}; 
+template<int SZ, bool EDGE = 1> struct HLD { //add all edges, then init
+  
+  //#define RANGE_UPD //range updates? uses LSEG if defined; otherwise SEG
+  
+  vector<int> adj[SZ], rpos; //rpos not used, but could be useful
+  int par[SZ], root[SZ], depth[SZ], siz[SZ], pos[SZ]; 
+  int ti;
+  
+  void add(int x, int y) { adj[x].pb(y), adj[y].pb(x); }
+  void init(int R = 0) {
+    par[R] = depth[R] = ti = 0; dfsSz(R); 
+    root[R] = R; dfsHld(R); 
+  }
+    
+  #ifndef RANGE_UPD
+    using T = int; 
+    SEG<T> tree; 
+    const T ID = tree.ID; 
+    T comb(T a, T b) { return tree.comb(a, b); }
+    HLD() { tree.init(SZ); }
+  #else
+    LSEG tree; using T = LSEG::Q;
+    const T ID = tree.id.second; 
+    T comb(T a, T b) { return tree.qop(a, LSEG::R(), b, LSEG::R()); } //T is query type
+    HLD() : tree(SZ) {}    
+  #endif
+  
+  void dfsSz(int x) { 
+    siz[x] = 1; 
+    trav(y, adj[x]) {
+      par[y] = x; depth[y] = depth[x]+1;
+      adj[y].erase(find(all(adj[y]), x)); //remove parent from adj list
+      dfsSz(y); siz[x] += siz[y];
+      if(siz[y] > siz[adj[x][0]]) swap(y, adj[x][0]);
+    }
+  }
+  void dfsHld(int x) {
+    pos[x] = ti++; rpos.pb(x);
+    trav(y,adj[x]) {
+      root[y] = (y == adj[x][0] ? root[x] : y);
+      dfsHld(y); 
+    }
+  }
+  
+  int lca(int x, int y) {
+    for(; root[x] != root[y]; y = par[root[y]])
+      if(depth[root[x]] > depth[root[y]]) swap(x,y);
+    return depth[x] < depth[y] ? x : y;
+  }
+  int dist(int x, int y) { //# edges on path
+    return depth[x] + depth[y] - 2 * depth[lca(x,y)]; 
+  }
+
+  template<typename fnc> void processPath(int x, int y, fnc&& op) {
+      for(; root[x] != root[y]; y = par[root[y]]) {
+        if(depth[root[x]] > depth[root[y]]) swap(x, y);
+        op(pos[root[y]], pos[y]); 
+      }
+      if(depth[x] > depth[y]) swap(x,y);
+      op(pos[x] + EDGE, pos[y]); 
+  }
+  
+  #ifdef RANGE_UPD
+    void updpath(int x, int y, int v) { 
+        processPath(x,y,[this,&v](int l, int r) { 
+        tree.upd(l,r,v); }); 
+    }
+  #else
+    void upd(int x, int v) { //update a single node 
+        processPath(x,x,[this,&v](int l, int r) { 
+        tree.upd(l,v); }); 
+    }
+  #endif
+  T qrypath(int x, int y) { 
+      T res = ID; processPath(x,y,[this,&res](int l, int r) { 
+      res = comb(res,tree.qry(l,r)); });
+      return res; 
+  }
+  #ifdef RANGE_UPD
+    void updsubtree(int x, int v) { 
+        tree.upd(pos[x] + EDGE, pos[x] + siz[x] - 1, v); 
+    }
+  #endif
+  T qrysubtree(int x) {
+      return tree.qry(pos[x] + EDGE, pos[x] + siz[x] - 1);
+  }
+};
+
+//x 1 0 0
+//c 2 a b
+int n,m; vector<pair<pii,pii>> qr; 
+HLD<_+5,0> h; 
 
 int main() {
-	IO("milkvisits");
-  s.init(all(v)); what(s.qry(0,2,1)); s.upd(0,10); what(s.qry(0,2,10));  
-  //trav(a, s.seg) out(all(a)); 
+	IO("");
   
+  out(mp(1,2)); out(mp(2,3)); exit(0); 
+  
+  in(n,m); F0R(i,n) { int x; in(x); qr.eb(mp(x,1),mp(i,0)); }
+  F0R(i,n-1) {
+    int x,y; in(x,y),--x,--y; 
+    h.add(x,y); 
+  }
+  h.init(); 
+  F0R(i,m) {
+    int a,b,c; in(a,b,c),--a,--b; 
+    qr.eb(mp(c,2),mp(a,b)); 
+  }
+  sort(all(qr)); 
+  trav(q,qr) {
+    if(q.f.s == 1) h.upd(q.s.f,q.f.f);
+    else {
+      //out_(h.qrypath(q.s.f,q.s.s) == q.f.f);
+    }
+    what(q);
+  } //NL; 
 }
 
 // // // // // // // // // // // // // // // // //
