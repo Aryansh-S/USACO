55b12fabe92d8b9a3cdd9173b071c4eb580b3b47
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-06-30 00:14:39 -0700

    Update fenwickmerge.cpp

--- algo & ds/fenwickmerge.cpp
+++ algo & ds/fenwickmerge.cpp
@@ -3,7 +3,7 @@
 template<class T, int SZ> struct MBIT { //merge sort bit (zero indexed!)
 	OST<pair<T, int>> val[SZ + 1]; 
 	void upd(int x, T y, int t = 1) { //0 del, 1 ins -- put y at index x
-    ++x;
+    		++x;
 		for(int X = x; X < SZ; X += X&-X) {
 			if(t == 1) val[X].ins({y,x});
 			else val[X].ers({y,x});
@@ -15,7 +15,7 @@ template<class T, int SZ> struct MBIT { //merge sort bit (zero indexed!)
 		return res;
 	}
 	int qry(int i, int j, T a, T b) { //in subarray [i,j], how many are in range [a,b]
-    ++i,++j;
+    		++i,++j;
 		return qryd(j,b)-qryd(j,a-1)-qryd(i-1,b)+qryd(i-1,a-1); 
 	}
 };
