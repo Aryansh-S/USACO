3c02e950bcc74404bb1813e80898e9c7aef0cbbf
Aryansh Shrivastava <63766132+Aryansh-S@users.noreply.github.com>
2022-06-24 17:51:38 -0700

    Update shell_game.cpp

--- USACO/2019/Jan/Bronze/shell_game.cpp
+++ USACO/2019/Jan/Bronze/shell_game.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int N; 
 int pts1, pts2, pts3;
-int has1[3], has2[3], has3[3];
+bool has1[3], has2[3], has3[3];
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
@@ -18,10 +18,10 @@ int main() {
 		--a, --b, --g; // zero index
 		swap(has1[a], has1[b]); 
 		swap(has2[a], has2[b]); 
-		swap(has3[a], has3[b]); 
-		if(has1[g] == 1) ++pts1; 
-		if(has2[g] == 1) ++pts2;
-		if(has3[g] == 1) ++pts3;
+		swap(has3[a], has3[b]);
+		if(has1[g]) ++pts1; 
+		if(has2[g]) ++pts2;
+		if(has3[g]) ++pts3;
 	}
 	cout << max({pts1, pts2, pts3}) << "\n";
 }
