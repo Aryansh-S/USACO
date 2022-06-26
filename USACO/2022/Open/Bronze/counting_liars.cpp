0af24da51a32d94465b6083211b214b858ab8582
Aryansh Shrivastava <63766132+Aryansh-S@users.noreply.github.com>
2022-06-25 17:37:50 -0700

    Create counting_liars.cpp

---
+++ USACO/2022/Open/Bronze/counting_liars.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int N; 
+vector<pair<char, int>> cow;
+vector<int> guess; 
+int ans = 1000;
+
+bool lying(int loc, pair<char, int> info) {
+	if (info.first == 'L') return loc > info.second; 
+	else return loc < info.second;
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin >> N; 
+	for (int i = 0; i < N; ++i) {
+		char dir; int loc; 
+		cin >> dir >> loc; 
+		cow.emplace_back(dir, loc);
+		for (int d: {-1, 0, 1}) guess.emplace_back(loc + d);
+	}
+	for (int i: guess) {
+		int tot = 0; 
+		for (auto p: cow) tot += lying(i, p);
+		ans = min(ans, tot);
+	}
+	cout << ans << "\n";
+}
