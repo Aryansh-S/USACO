8a15f66b3f373cfc2da9408afaa19b06c1b574b8
Aryansh Shrivastava <63766132+Aryansh-S@users.noreply.github.com>
2022-07-09 18:41:18 -0700

    update spacing

--- algo_by_div/bronze.cpp
+++ algo_by_div/bronze.cpp
@@ -4,19 +4,19 @@
 void re(vector<int> &v) {
 	int n; cin >> n; 
 	v.resize(n); 
-	for(int &t: v) cin >> t;
+	for (int &t: v) cin >> t;
 }
 
 // O(n) write data
 void pr(const vector<int> &v) {
 	string s = "";
-	for(int t: v) cout << s << t, s = " ";
+	for (int t: v) cout << s << t, s = " ";
 	cout << "\n";
 }
 
 // O(n) complete search (returns leftmost idx if found and -1 otherwise)
 int csearch(const vector<int> &v, const int x) {
-	for(int i = 0; i < size(v); ++i) if(v[i] == x) return i; 
+	for (int i = 0; i < size(v); ++i) if (v[i] == x) return i; 
 	return -1;
 }
 
@@ -26,14 +26,14 @@ bool ckmin(auto &a, const auto b) { return a > b ? a = b, 1 : 0; }
 // O(n) running tally (e.g., min element overall)
 int rmin(const vector<int> &v) {
 	int m = v[0]; 
-	for(int t: v) ckmin(m, t); 
+	for (int t: v) ckmin(m, t); 
 	return m;
 }
 
 // O(n) get reverse index
 auto ridx(const vector<int> &v) {
 	vector<int> rv(2e5 + 5);
-	for(int i = 0; i < size(v); ++i) rv[v[i]] = i;
+	for (int i = 0; i < size(v); ++i) rv[v[i]] = i;
 	return rv;
 }
 
@@ -41,7 +41,7 @@ auto ridx(const vector<int> &v) {
 void brsort(vector<int> &v) {
     for(int i = 0; i < size(v); ++i) {
         int idx = i; 
-        for(int j = i; j < size(v); ++j) if(v[idx] > v[j]) idx = j;
+        for (int j = i; j < size(v); ++j) if(v[idx] > v[j]) idx = j;
         v.insert(begin(v) + i, v[idx]);
         v.erase(begin(v) + idx + 1);
     }
@@ -49,7 +49,7 @@ void brsort(vector<int> &v) {
 
 // O(n) recursion
 void recur(const int n, int it = 0) {
-	if(it < n) {
+	if (it < n) {
 		cout << it << "\n";
 		recur(n, ++it);
 	}
@@ -58,7 +58,7 @@ void recur(const int n, int it = 0) {
 // O(2^n) generate all subsets recursively
 vector<int> subset;
 void sub(const vector<int> &v, int idx = 0) {
-	if(idx == size(v)) {
+	if (idx == size(v)) {
 		// process subset
 	}
 	sub(v, idx + 1); 
@@ -72,7 +72,7 @@ void perm(vector<int> v) {
 	sort(begin(v), end(v)); 
 	do {
 		// process permutation
-	} while(next_permutation(begin(v), end(v)));
+	} while (next_permutation(begin(v), end(v)));
 }
 
 // O(n log n) count distinct elements
