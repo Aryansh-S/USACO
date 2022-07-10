66d4ff37230a539e64f5a80096007747ae5ac3f4
Aryansh Shrivastava <63766132+Aryansh-S@users.noreply.github.com>
2022-07-10 16:39:14 -0700

    Create silver.cpp

---
+++ algo_by_div/silver.cpp
@@ -0,0 +1,29 @@
+// a comprehensive list of silver algorithm/data structure implementations 
+
+
+
+// the below are essential for graph problems (optional for bronze but necessary for silver):
+
+int n, m; // # nodes, # edges
+vector<vector<int>> adj; // store graph as adjacency list
+vector<bool> vis; // keep track of which nodes visited for dfs
+
+// O(m), construct graph in adjacency list format from edges
+void read_adj_list() {
+	adj.resize(n), vis.resize(n); 
+	cin >> m;
+	for (int i = 0; i < m; ++i) {
+		int a, b; // nodes in edge
+		cin >> a >> b; 
+		--a, --b; // zero idx nodes
+		adj[a].emplace_back(b); // adds directed edge a->b
+		// adj[b].emplace_back(a); // uncomment if undirected edge (adds directed edge b->a)
+	}
+}
+
+// O(n + m), do depth first search (dfs) of graph modeled by adjacency list
+void dfs(int v) { // initiate by calling dfs(starting_node)
+	vis[v] = 1;
+	// add code here to process v (remember that v + 1 is the actual node in problem bc we zero idx)
+	for (int u: adj[v]) if (!vis[u]) dfs(u); 
+}
