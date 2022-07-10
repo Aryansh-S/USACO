84b893ec2904786f7e21189564c5c00e5a13efba
Aryansh Shrivastava <63766132+Aryansh-S@users.noreply.github.com>
2022-07-09 19:26:52 -0700

    Create sleepy_cow_sorting.cpp

---
+++ USACO/2019/Jan/Bronze/sleepy_cow_sorting.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+// compute the longest sorted suffix
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0); 
+	freopen("sleepy.in", "r", stdin); 
+	freopen("sleepy.out", "w", stdout); 
+
+	int n; 
+	cin >> n; 
+	vector<int> v(n); 
+	for (int &t: v) cin >> t; 
+	for (int i = n - 1; i > 0; --i)
+		if (v[i - 1] > v[i]) { cout << i << "\n"; exit(0); }
+	cout << 0 << "\n";
+}
