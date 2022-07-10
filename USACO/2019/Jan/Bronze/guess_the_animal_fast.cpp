7a7a77d9b5f9ade7c394f3d303ce5d112d83b08e
Aryansh Shrivastava <63766132+Aryansh-S@users.noreply.github.com>
2022-07-09 19:03:03 -0700

    Update guess_the_animal_fast.cpp

--- USACO/2019/Jan/Bronze/guess_the_animal_fast.cpp
+++ USACO/2019/Jan/Bronze/guess_the_animal_fast.cpp
@@ -5,10 +5,10 @@ int N;
 vector<vector<string>> prop;
 int ans = 0;
 
-int common(auto v1, auto v2) { // finds num common in O(n log n) rather than O(n^2)
+int common(auto v1, auto v2) { // finds num common in O(n log n) rather than O(n^2), using merging and sorting 
 	// PIE: |v1 isect v2| = |v1| + |v2| - |v1 union v2|
 	int tot = size(v1) + size(v2);
-	// now, we will change v2 into v1 union v2
+	// now, we will change v2 into v1 union v2 by merging the vectors and keeping only distinct elements
 	for (auto i: v1) v2.emplace_back(i); 
 	sort(begin(v2), end(v2)); 
 	v2.erase(unique(begin(v2), end(v2)), end(v2));
