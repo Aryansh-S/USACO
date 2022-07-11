66a6375a8a4656c463b935d96fa1e232a4c1970a
Aryansh Shrivastava <63766132+Aryansh-S@users.noreply.github.com>
2022-07-11 16:44:32 -0700

    Update silver.cpp

--- algo_by_div/silver.cpp
+++ algo_by_div/silver.cpp
@@ -3,13 +3,13 @@
 
 
 // the below are essential for graph problems (optional for bronze but necessary for silver)
-// all of these are for adjacency lists (used for sparse graphs, most common on USACO) but can be easily modified for adjacency matrices if needed
+// all of these are for adjacency lists but can be easily modified for adjacency matrices if needed
 
 int n, m; // # nodes, # edges
 vector<vector<int>> adj; // store graph as adjacency list
 bool vis[(int)(2e5 + 5)]; // keep track of which nodes visited for dfs // put at least max node + 1 in ()
 
-// O(n + m), construct graph in adjacency list format from edges
+// O(n + m) construct graph in adjacency list format from edges
 void read_adj_list() {
 	adj.resize(n);
 	for (int i = 0; i < m; ++i) {
@@ -21,14 +21,20 @@ void read_adj_list() {
 	}
 }
 
-// O(n + m), do depth first search (dfs) of graph modeled by adjacency list
+// O(n + m) do depth first search (dfs) of graph modeled by adjacency list
 void dfs(int v) { // initiate by calling dfs(starting_node)
 	vis[v] = 1;
 	// add code here to process v (remember that v + 1 is the actual node in problem bc we zero idx)
 	for (int u: adj[v]) if (!vis[u]) dfs(u); 
 }
 
-// O(n + m), iterative dfs (optional but instructive on the use of stacks and a parallel for how bfs will use queues later)
+// O(n + m) floodfill
+void flood() {
+	for (int i = 0; i < n; ++i) if (!vis[i]) dfs(i); 
+	// visits all nodes in graph by dfsing from each node not visited
+}
+
+// O(n + m) iterative dfs (optional but instructive on the use of stacks and a parallel for how bfs will use queues later)
 void dfs_it(int start) {
 	stack<int> todo; 
 	todo.emplace(start); 
