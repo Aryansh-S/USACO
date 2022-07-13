b8a8da25ec27ac66d11e5d1afa8f36dbbb19fdd6
Aryansh Shrivastava <63766132+Aryansh-S@users.noreply.github.com>
2022-07-13 14:49:11 -0700

    Update gold.cpp

--- algo_by_div/gold.cpp
+++ algo_by_div/gold.cpp
@@ -156,11 +156,11 @@ int spfa(int start, int end) {
 
 // O(m log m) kruskal minimum spanning tree (mst) using dsu
 int kruskal() {
-  int mst = 0; 
   vector<array<int, 3>> edges; // (weight, start_node, end_node)
-  dsu trees; trees.init(n); 
   for (int i = 0; i < n; ++i) for (auto [w, j]: adj[i]) edges.emplace_back(array{w, i, j});
-  sort(begin(edges), end(edges)); 
+  sort(begin(edges), end(edges));
+  dsu trees; trees.init(n); 
+  int mst = 0;
   for (auto [w, i, j]: edges) if (trees.unite(i, j)) mst += w; 
   return mst; 
 }
