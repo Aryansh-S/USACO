c134f3918e827c25eade79d3a241a791411f6d77
Aryansh Shrivastava <63766132+Aryansh-S@users.noreply.github.com>
2022-07-12 19:41:05 -0700

    Update silver.cpp

--- algo_by_div/silver.cpp
+++ algo_by_div/silver.cpp
@@ -4,7 +4,7 @@
 // in practice, you may also use arrays of max variable size.
 // also, you may have to replace ints with long longs to prevent overflow in some cases
 
-// O(n) two pointer search to find pair of indices satisfying a condition
+// O(n) two pointer search to find pair of indices in sorted array that satisfy a condition
 array<int, 2> two_pointers(const vector<int> &v, int sum) {
 	int l = 0, r = size(v) - 1; 
 	while (l < r) {
