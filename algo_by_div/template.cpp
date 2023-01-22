256a1799ae4094fd70ecf6198ca65157134f4cd4
Aryansh Shrivastava <63766132+Aryansh-S@users.noreply.github.com>
2023-01-22 15:16:29 -0800

    Create template.cpp

---
+++ algo_by_div/template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <ext/pb_ds/tree_policy.hpp>
+// #include <ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
+// template<class T, class U = null_type, class chash = hash<T>> using hset = gp_hash_table<T, U, chash>;
+// template<class T, class U = null_type, class cmp = less<T>> using oset = tree<T, U, cmp, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  
+}
